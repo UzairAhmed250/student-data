f3a079680d0227a89ba2e4acea51b86969bedd26
uzairahmed250 <uzair2500231@gmail.com>
2025-03-21 01:47:06 +0500

    adding create student data funcnality

--- src/CreateStudent.js
+++ src/CreateStudent.js
@@ -1,10 +1,25 @@
 import React, { useEffect, useState } from 'react';
 import "./App.css";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
+
 
 function CreateStudent() {
+    const navigate = useNavigate();
+    const [validationError, setValidationError] = useState({
+        name: false,
+        place: false,
+        phone: false
+    });
+    const validatoinErrorChange = (e) => {
+        setValidationError({
+            ...validationError,
+            [e.target.name]: true
+        })
+    }
+
     const [userValue,setUserValue] = useState({
-        id:"",
+        id: uuidv4(),
         name:"",
         phone:"",
         place:""
@@ -22,32 +37,54 @@ function CreateStudent() {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        // console.log(id, name, place, phone);
         console.log(userValue);
-        
-        // const id = e.target.id.value;
-        // const name = e.target.name.value;
-        // const place = e.target.place.value;
-        // const phone = e.target.phone.value;
-        // const student = { id, name, place, phone };
-        // fetch('http://localhost:8000/students', {
-        //     method: 'POST',
-        //     headers: { "Content-Type": "application/json" },
-        //     body: JSON.stringify(student)
-        // }).then(() => {
-        //     console.log('new student added');
-        // })
+        fetch("http://localhost:8000/students",{
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(userValue)
+        })
+        .then((res)=>
+        {
+            alert("Student Data Updated Successfully!");
+            navigate('/');
+        })
+        .catch((err)=>console.log(err))
     }
 
-     const response = fetch('http://localhost:8000/students', 
-         {
-        method: 'POST',
-        body: JSON.stringify(userValue),
-        headers: {
-            "Content-Type" : "application/json"
-        }
-    });
-    console.log(response, "response11");
+    // async function getData(){
+    //     const url = "http://localhost:8000/students";
+        
+    //     console.log(url, "url'");
+    //     try {
+    //         const response = await fetch(url,{
+    //             method: 'POST',
+    //             headers: {
+    //                 'Content-Type': 'application/json'
+    //             },
+    //             body: JSON.stringify({username: "name"}),
+
+    //         });
+            
+    //         if(!response.ok){
+    //             throw new Error(`response status: ${response.status}`);
+    //         }
+    //         const json = await response.json();
+    //         console.log(json);
+    //     } catch (error) {
+    //         console.log(error.message);
+    //     }
+    // }
+
+
+    //  const response = fetch('http://localhost:8000/students', 
+    //      {
+    //     method: 'POST',
+    //     body: JSON.stringify(userValue),
+    //     headers: {
+    //         "Content-Type" : "application/json"
+    //     }
+    // });
+    // console.log(response, "response11");
   return (
 
 
@@ -59,20 +96,25 @@ function CreateStudent() {
             <form onSubmit={handleSubmit} className="form-fields" >
             <div className='form-group'>
                     <label htmlFor="id">Id: </label>
-                    <input type="text" name="id" id="id" value={userValue.id} onChange={handleFormChane} className='form-control' required/>
+                    <input type="text" name="id" id="id" value={userValue.id} onChange={handleFormChane} className='form-control' disabled required/>
                 </div>
                 <div className='form-group'>
                     <label htmlFor="name">Student Name: </label>
                     <input type="text" name="name" id="name" value={userValue.name} onChange={(e) =>handleFormChane(e)}
-                    className='form-control' required/>
+                    onMouseDown={(e)=>validatoinErrorChange(e)}  className='form-control' required/>
+                    {userValue.name.length === 0 && validationError.name && <span className='error'>* Name is required</span>}
                 </div>
                 <div className='form-group'>
                     <label htmlFor="place">Place: </label>
-                    <input type="text" name="place" id="place" value={userValue.place} onChange={(e) => handleFormChane(e)} className='form-control' required/>
+                    <input type="text" name="place" id="place" value={userValue.place} onChange={(e) => handleFormChane(e)}  onMouseDown={(e)=>validatoinErrorChange(e)} className='form-control' required/>
+                    {userValue.place.length === 0 && validationError.place && <span className='error'>* place is required</span>}
+
                 </div>
                 <div className='form-group'>
                     <label htmlFor="phone">Phone: </label>
-                    <input type="text" name="phone" id="phone" value={userValue.phone} onChange={(e) => handleFormChane(e)} className='form-control' required/>
+                    <input type="text" name="phone" id="phone" value={userValue.phone} onChange={(e) => handleFormChane(e)}  onMouseDown={(e)=>validatoinErrorChange(e)} className='form-control' required/>
+                    {userValue.phone.length === 0 && validationError.phone && <span className='error'>* phone is required</span>}
+
                 </div>
                 <div className='form-group form-group-button'>
                     <button type="submit" className='btn btn-primary'>Add Student</button>
