4e7db4362c12627749afc3ed42b79741d9e0c2f9
uzairahmed250 <uzair2500231@gmail.com>
2025-05-03 16:05:51 +0500

    updateed-meta-tag

--- src/ViewStudentDetails.js
+++ src/ViewStudentDetails.js
@@ -1,15 +1,36 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom';
+import { doc , getDoc} from "firebase/firestore";
+import db from './configuration';
+
 
 function ViewStudentDetails() {
   const {studentid} = useParams()
   const [studentDetails, setStudentDetails] = useState({});
+  
+  const handleStudentDetail = async() => {
+    const docRef = doc(db, studentid);
+    const docSnap = await getDoc(docRef);
+    console.log(docSnap, "mm")
+
+    if (docRef.exist()) {
+      console.log("DD", docSnap.data());
+    } else{
+      console.log("no such document!")
+    }
+
+
+  }
+  
   useEffect(() =>{
-    fetch("http://localhost:8000/students/"+studentid)
-    .then((res)=>res.json())
-    .then((data)=>setStudentDetails(data))
-    .catch((err)=>console.log(err.message))
+        // fetch("http://localhost:8000/students/"+studentid)
+    // .then((res)=>res.json())
+    // .then((data)=>setStudentDetails(data))
+    // .catch((err)=>console.log(err.message))
+    handleStudentDetail()
   },[])
+
+  console.log(studentDetails, "studentDetails" )
 
   return (
     <div className='container'>
