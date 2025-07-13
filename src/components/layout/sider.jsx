b8e9085e26523d70199a14b7e53c2f65f618bef2
uzairahmed250 <uzair2500231@gmail.com>
2025-07-13 16:08:23 +0500

    layout-in-process

--- src/components/layout/sider.jsx
+++ src/components/layout/sider.jsx
@@ -8,11 +8,13 @@ import {
 } from '@ant-design/icons';
 import { Button, Menu } from 'antd';
 import { Link } from 'react-router-dom';
+
+
 const items = [
-  { key: '1', icon: <UsergroupAddOutlined />, label: 'Add Student', Link: "/StudentTable"   },
-  { key: '2', icon: <EditOutlined />, label: 'Edit Student' },
-  { key: '3', icon: <UserOutlined />, label: 'Student Detail' },
-  { key: '4', icon: <UserOutlined />, label: 'View Student Detail' },
+  { key: '1', icon: <UsergroupAddOutlined />, label: <Link to="/student/create"> Add Student </Link>},
+  { key: '2', icon: <EditOutlined />, label: <Link to="/student/edit/:studentid">Edit Student </Link> },
+  { key: '3', icon: <UserOutlined />, label: <Link to="/student/create">Student Detail </Link> },
+  { key: '4', icon: <UserOutlined />, label: <Link to="/StudentTable">View Student Detail </Link> },
 //   { key: '5', icon: <ContainerOutlined />, label: 'Option 3' },
 
 ];
@@ -21,18 +23,33 @@ const Sider = () => {
   const toggleCollapsed = () => {
     setCollapsed(!collapsed);
   };
+
   return (
-    <div style={{ width: 256 ,}}>
-      <Button type="primary" onClick={toggleCollapsed} style={{ marginBottom: 16 }}>
+    <div style={{ width: collapsed ? 8 : 256 , background: "white", transition: "0.3s"}}>
+      <div 
+        style={{
+          // display: "flex",
+          // justifyContent: "center",
+          // alignItems: "center",
+          // height: 64, // adjust if needed
+          width: "100% !important",
+          background: "white",
+          // borderBottom: "1px solid #f0f0f0"
+        }}
+      >
+
+      <Button type="primary" onClick={toggleCollapsed} style={{ marginBottom: 0 }}>
         {collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
       </Button>
+      </div>
       <Menu
         defaultSelectedKeys={['1']}
-        defaultOpenKeys={['sub1']}
+        // defaultOpenKeys={['sub1']}
         mode="inline"
-        theme="dark"
+        theme="light"
         inlineCollapsed={collapsed}
         items={items}
+        style={{height: "100vh", padding: 0, margin: 0,}}
       />
     </div>
   );
